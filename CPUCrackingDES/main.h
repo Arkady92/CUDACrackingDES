61271e0ef9bce313aa2578050d5134f86a3bfa80
Arkady92 <abelskij@student.mini.pw.edu.pl>
2014-06-06 17:21:57 +0200

    initial CPU code

---
+++ CPUCrackingDES/main.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+#define KEYLEN 64
+#define PC1LEN 56
+#define PC2LEN 48
+#define SHIFTSLEN 16
+#define BLOCKSLEN PC1LEN/2
+#define KEYCOUNT 16
+#define MSGLEN 64
+#define IPMSGCOUNT 16
+#define EXTENDEDLEN 48
+#define SBOXCOUNT 8
+#define SBOXSIZE 4
+#define SBLOCKSIZE 6
+#define SCOLUMNS 16
+#define SROWS 4
+int main();
